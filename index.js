53cd3eed614325dc3551f2a1e3df2eba1e8aa7cb
sauerdev <davidemanuelsauer@gmail.com>
2024-10-23 16:51:52 -0300

    ATUALIZAÇÃO 23/10
    CORREÇÃO DE ERROS

--- index.js
+++ index.js
@@ -19,22 +19,22 @@ app.use(express.json());
 let cartoes = [
     {
         nome: 'cartao 1',
-        valor: '10pila',
+        descricao: '10pila',
         imagem: 'https://www.sigmaaldrich.com/content/dam/sigma-aldrich/product0/071/a5752-25mg_0057134_btl.tif/_jcr_content/renditions/a5752-25mg_0057134_btl-large'
     },
     {
         nome: 'cartao 2',
-        valor: '48,89cents',
+        descricao: '48,89cents',
         imagem: 'https://images-americanas.b2w.io/produtos/01/00/img/3769059/8/3769059896_1GG.jpg'
     },
     {
         nome: 'cartao 3',
-        valor: 'fiftcent',
+        descricao: 'fiftcent',
         imagem: 'https://cdn.awsli.com.br/600x450/1810/1810043/produto/88385637/c76fbaa43e.jpg'
     },
     {
         nome: 'cartao 4',
-        valor: '2dól',
+        descricao: '2dól',
         imagem: 'https://cdn.sistemawbuy.com.br/arquivos/3d031251600db30f801738111450cdd4/produtos/65553c13c7c0c/20240205_170005-1-65c37c57574dc.jpg'
     },
 ];
@@ -46,18 +46,12 @@ app.get('/cartoes', (req, res) => {
 })
 
 app.post('/cartoes', (req, res) => {
-    const{nome , valor , imagem} = req.body;
+    const{nome , descricao , imagem} = req.body;
 
-    cartoes.push({nome: nome, valor: valor, imagem: imagem});
+    cartoes.push({nome: nome, descricao: descricao, imagem: imagem});
     console.log(cartoes);
     res.status(201).json({ mensagem: 'funfo' });
 });
-
-app.get('/cartoes', (req, res) => {
-    res.status(200).json({ cartoes });
-    console.log('oi');
-
-})
 
 app.delete('/cartoes', (req, res) =>{
  const numero = req.body.numero
@@ -74,10 +68,6 @@ app.put('/cartoes', (req, res) => {
     res.status(201).json({ mensagem: 'funfo o put' })
 })
 
-app.get('/cartoes', (req, res) => {
-    res.status(200).json({ mensagem: 'Olá Mundo!' });
-    console.log('Oi');
-});
 
 app.listen(porta, () => {
     console.log(`Servidor rodando na porta ${porta}`);
